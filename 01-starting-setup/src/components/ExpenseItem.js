8d142ffbce7443d7e2e91f32521ceba742f6e93e
SSG\212573 <jhlee9091@gmail.com>
2023-03-23 19:24:34 +0900

    Intialize

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 23th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
